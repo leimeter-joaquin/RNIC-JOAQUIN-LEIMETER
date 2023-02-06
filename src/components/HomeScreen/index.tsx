5772080dbe399c602004a5b0754490b91aeb712c
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-02-06 14:34:48 -0300

    RNIC-W03: add dynamic styles for android and ios - keyboard and form functionality

--- src/components/HomeScreen/index.tsx
+++ src/components/HomeScreen/index.tsx
@@ -1,23 +1,52 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, View, StatusBar, Text, FlatList} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  View,
+  StatusBar,
+  Text,
+  TouchableHighlight,
+  TextInput,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+  AppState,
+} from 'react-native';
 import styles from './styles';
 import tasksArray from '../../constants/mock';
 import {Task} from '../../types/tasks';
 import Card from '../Card';
+import moveFocusTo from '../../utils/moveFocusTo';
+
+const EmptyList = () => <Text>EmptyList</Text>;
 
 function HomeScreen(): JSX.Element {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+
+  const isAndroid = Platform.OS === 'android';
 
   useEffect(() => {
     if (tasksArray) {
       setTasks(tasksArray);
     }
+
+    const appStateSubscription = AppState.addEventListener(
+      'change',
+      nextAppState => {
+        if (nextAppState !== 'active') {
+          setTasks(tasksArray);
+        }
+      },
+    );
+
+    return () => {
+      appStateSubscription.remove();
+    };
   }, []);
 
-  /**
-   *
-   * @param id id of the task to modify
-   */
   const setTaskStatus = (id: string) => {
     setTasks(prevState => {
       const newTasks = prevState.map(task => {
@@ -31,24 +60,71 @@ function HomeScreen(): JSX.Element {
     });
   };
 
+  const addTask = () => {
+    if (newTitle || newDescription) {
+      return;
+    }
+
+    setTasks(prevState => {
+      const newTasks: Task[] = [
+        ...prevState,
+        {
+          id: Date.now().toString(),
+          title: newTitle,
+          description: newDescription,
+          done: false,
+        },
+      ];
+
+      setNewTitle('');
+      setNewDescription('');
+
+      Keyboard.dismiss();
+
+      return newTasks;
+    });
+  };
+
+  const secondInput = useRef<TextInput>(null);
+
   return (
-    <SafeAreaView>
-      <StatusBar backgroundColor="#61dafb" />
-
-      <FlatList
-        data={tasks}
-        renderItem={({item}) => (
-          <Card task={item} setTaskStatus={setTaskStatus} />
-        )}
-      />
-
-      <View>
-        <Text style={styles.greetings}>Hello</Text>
-      </View>
-
-      <View>
-        <Text>{JSON.stringify(tasks, null, 2)}</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={styles.container}>
+        <StatusBar barStyle={isAndroid ? 'light-content' : 'dark-content'} />
+        <FlatList
+          contentContainerStyle={styles.cardsContainer}
+          data={tasks}
+          renderItem={({item}) => (
+            <Card task={item} setTaskStatus={setTaskStatus} />
+          )}
+          ListEmptyComponent={() => EmptyList()}
+        />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {/* I had to wrap the inputs and button on a view because this component wouldn't let me have multiple children nodes. */}
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              value={newTitle}
+              onChangeText={setNewTitle}
+              returnKeyType="next"
+              blurOnSubmit={false}
+              onSubmitEditing={() => moveFocusTo(secondInput)}
+            />
+            <TextInput
+              ref={secondInput}
+              style={styles.input}
+              value={newDescription}
+              returnKeyType="next"
+              onChangeText={setNewDescription}
+            />
+            <TouchableHighlight style={styles.button} onPress={addTask}>
+              <Text style={styles.buttonText}>Press</Text>
+            </TouchableHighlight>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
